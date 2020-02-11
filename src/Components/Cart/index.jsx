7f7060841504a210e7c8ad8fde4e9c575c4230df
konikowska <katarzyna.konikowska@gmail.com>
2020-02-11 12:40:27 +0100

    basic config

---
+++ src/Components/Cart/index.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+const Cart = () => {
+  return (
+    <div>
+      <p>Cart Container</p>
+    </div>
+  )
+}
+export default Cart
---
+++ src/Components/Product/index.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+      <p>Product Container</p>
+    </div>
+  )
+}
+export default Product
